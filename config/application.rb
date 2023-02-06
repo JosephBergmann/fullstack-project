1de1fbc7167768c362de90f8e6d9660629d776be
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2023-02-06 15:38:02 -0500

    deploying to render?

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,8 @@ module SnackOverflow
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.railties_order = [:all, :main_app]
+    
     config.api_only = true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
     resource :session, only: [:create, :show, :destroy]
   end
 
+  get '*path', to: "static_pages#frontend_index"
 end
