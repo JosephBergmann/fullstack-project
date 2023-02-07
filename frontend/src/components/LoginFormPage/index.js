4dbeaac6e44a49e60d06be286fcaafe296fe7a45
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2023-02-07 13:55:43 -0500

    added indexformpage

---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import * as sessionActions from '../../store/session';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+
+const IndexFormPage = () => {
+    const dispatch = useDispatch();
+    const sessionUser = useSelector(state => state.session.user);
+    const[email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState([]);
+
+    if(sessionUser) return <Redirect to="/"  />
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        setErrors([]);
+
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <ul>
+                {errors.map(error => <li key={error}>{error}</li>)}
+            </ul>
+            <label>
+                Email
+                <input type="text"
+                    value={email} onChange={(e) => setEmail(e.target.value)} required />
+            </label>
+            <label>
+                Password
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+            </label>
+            <button type="submit">Log In</button>
+        </form>
+    )
+}
+export default IndexFormPage;
