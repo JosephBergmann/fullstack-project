db7151fd5737a14f923e6f284df1763f5714cd7f
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2023-02-08 23:48:43 -0500

    added stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,21 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import IndexFormPage from './components/LoginFormPage'
 import SignupFormPage from './components/SignupFormPage'
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <Switch>
-        <Route path="/login">
-          <IndexFormPage />
-        </Route>
-        <Route path="/signup">
-          <SignupFormPage />
-        </Route>
-    </Switch>
+    <>
+    <Navigation />
+      <Switch>
+          <Route path="/login">
+            <IndexFormPage />
+          </Route>
+          <Route path="/signup">
+            <SignupFormPage />
+          </Route>
+      </Switch>
+    </>
   );
 }
 
