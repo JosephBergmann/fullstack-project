e18d0da2b7f9f68e7e557fe9505f422b77335774
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2023-02-13 17:29:22 -0500

    added more styling

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -1,17 +1,23 @@
 import './SplashPage.css'
+import { NavLink } from 'react-router-dom';
 
 const SplashPage = () => {
     return (
-        <div>
+        <div className="outer-box">
             <div className="inner-boxes">
                 <div className="two-boxes">
                     <p className="box1">
                         Find the best answer to your technical question, help others answer theirs
+                        <NavLink to="/login">Join the community</NavLink>        
                     </p>
+                    
                     <p className="box2">
                         Want a secure, private space for your technical knowledge?
                     </p>
                 </div>
+                <p>
+                    Every developer needs a tab open to Stack Overflow
+                </p>
             </div>
         </div>
     )
