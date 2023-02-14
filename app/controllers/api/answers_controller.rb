9baf083082adf330887806672ff7cc9a6e472db7
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2023-02-14 11:14:18 -0500

    added a cursory create function, unsure of functionality

--- app/controllers/api/answers_controller.rb
+++ app/controllers/api/answers_controller.rb
@@ -3,11 +3,22 @@ class Api::AnswersController < ApplicationController
     end
 
     def create
+        @answer = Answer.new(answer_params)
+        if @answer.save
+            render json: @answer
+        else
+            render json: {error: 'you messed up'}
+        end
     end
 
     def update
     end
 
     def destroy
+    end
+
+    private 
+    def answer_params
+        params.require(:answer).permit(:poster_id, :question_id, :body)
     end
 end
