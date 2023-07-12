d7e4581159bf61f3bebe9900f7d2af9036a1d037
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2023-07-12 11:32:30 -0400

    added missing comment

--- db/migrate/20230214152233_create_answers.rb
+++ db/migrate/20230214152233_create_answers.rb
@@ -1,7 +1,7 @@
 class CreateAnswers < ActiveRecord::Migration[7.0]
   def change
     create_table :answers do |t|
-      t.references :poster, null: false, index: true foreign_key: {to_table: :users}
+      t.references :poster, null: false, index: true, foreign_key: {to_table: :users}
       t.references :question, null: false, index: true, foreign_key: {to_table: :questions}
       t.text :body, null: false
       t.timestamps
