ef4b30587b261f6a0edb6ff6d16cb5f68475054f
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2023-08-26 23:50:05 -0400

    added comments

--- frontend/src/components/ShowQuestion/AnswersList.js
+++ frontend/src/components/ShowQuestion/AnswersList.js
@@ -16,6 +16,7 @@ const AnswersList = () => {
     })
 
     const dispatch = useDispatch();
+    //CONSIDER DELETION
     // useEffect(() => {
     //     const unsubscribe = dispatch(createAnswer());
     //     return () => unsubscribe();
