2d50bd7cc9180b85efdea625ec9cba046a68b133
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2023-09-02 23:55:32 -0400

    added const functions

--- frontend/src/components/Navigation/SearchBar.js
+++ frontend/src/components/Navigation/SearchBar.js
@@ -2,6 +2,9 @@ import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 
 function SearchBar(){
+    const { query, setQuery } = useState('');
+    const history = useHistory();
+    const dispatch = useDispatch();
 
 }
 
