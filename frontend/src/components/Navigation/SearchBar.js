221ae8821c5bf63f66d4a5129883ba70176f516e
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2023-09-02 23:58:26 -0400

    fixed mistake relating to wrong storage of objects

--- frontend/src/components/Navigation/SearchBar.js
+++ frontend/src/components/Navigation/SearchBar.js
@@ -2,9 +2,11 @@ import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 
 function SearchBar(){
-    const { query, setQuery } = useState('');
+    const [query, setQuery] = useState('');
     const history = useHistory();
     const dispatch = useDispatch();
+
+
 
 }
 
