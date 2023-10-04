a3fb31278488018d095527e0e993977e466d5065
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2023-10-03 23:32:18 -0400

    new migration

--- db/migrate/20231001034003_finalize_polymorphic_votes_table.rb
+++ db/migrate/20231001034003_finalize_polymorphic_votes_table.rb
@@ -1,5 +1,6 @@
 class FinalizePolymorphicVotesTable < ActiveRecord::Migration[7.0]
   def change
-
+    remove_column :votes, :votable_id
+    add_reference :votes, :votable, polymorphic: true 
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_30_035656) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_01_034003) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,16 +48,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_30_035656) do
 
   create_table "votes", force: :cascade do |t|
     t.bigint "user_id"
-    t.bigint "votable_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "value"
+    t.string "votable_type"
+    t.bigint "votable_id"
     t.index ["user_id"], name: "index_votes_on_user_id"
+    t.index ["votable_type", "votable_id"], name: "index_votes_on_votable"
   end
 
   add_foreign_key "answers", "questions"
   add_foreign_key "answers", "users", column: "poster_id"
   add_foreign_key "questions", "users", column: "poster_id"
-  add_foreign_key "votes", "questions", column: "votable_id"
   add_foreign_key "votes", "users"
 end
